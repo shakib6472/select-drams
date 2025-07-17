b4cd1625a129f4ed48588557383fd4616b02b7c9
shakib6472 <shakibhasan3831@gmail.com>
2025-07-17 06:00:53 +0600

    Format and clean up the header comments in select-drams.php for improved readability

--- select-drams.php
+++ select-drams.php
@@ -1,26 +1,26 @@
-<?php 
+<?php
 
 /*
- * Plugin Name:      Select Drams Picklist
-* Plugin URI:        https://github.com/shakib6472/
-* Description:       A Plugin to upload warehouse data to WooCommerce. & Generate Picklist.
-* Version:           1.0.0
-* Requires at least: 5.2
-* Requires PHP:      7.2
-* Author:            Shakib Shown
-* Author URI:        https://github.com/shakib6472/
-* License:           GPL v2 or later
-* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
-* Text Domain:       select-drams
-* Domain Path:       /languages
-*/ 
-  
+ * Plugin Name:       Select Drams Picklist
+ * Plugin URI:        https://github.com/shakib6472/
+ * Description:       A Plugin to upload warehouse data to WooCommerce. & Generate Picklist.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Shakib Shown
+ * Author URI:        https://github.com/shakib6472/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       select-drams
+ * Domain Path:       /languages
+ */
+
 
 namespace SelectDrams;
 if (!defined('ABSPATH')) {
-exit; // Exit if accessed directly.
+    exit; // Exit if accessed directly.
 }
-  
+
 // Define plugin constants
 define('SELECT_DRAMS_VERSION', '1.0.0');
 define('SELECT_DRAMS_PLUGIN_DIR', plugin_dir_path(__FILE__));
@@ -31,3 +31,6 @@ require_once SELECT_DRAMS_PLUGIN_DIR . 'classes/main.php';
 
 // Initialize the plugin
 new Main();
+
+
+
